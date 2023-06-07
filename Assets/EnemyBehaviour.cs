98ce2ec039232979d6370befccd8b72353db8b8e
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2023-06-06 23:02:19 -0500

    Update2

--- Assets/EnemyBehaviour.cs
+++ Assets/EnemyBehaviour.cs
@@ -5,26 +5,38 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     [SerializeField] Transform player;
-    public Vector3 v0;
-    public float time;
-    public Vector3 aceleracion;
+    public float TimeToArrive=1f;
+    public float CurrentTime=0;
+    public float aceleracion;
+    private float Vo;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb= GetComponent<Rigidbody>();  
+        rb= GetComponent<Rigidbody>();
+        Vo = CalculateVelocidadInicial();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 posicionDeseada = player.position+ v0*time+(aceleracion*time*time)/2;
-        transform.position = Vector3.MoveTowards(transform.position, posicionDeseada, Time.deltaTime);
-        time += Time.deltaTime;
-
+        if(CurrentTime < TimeToArrive)
+        {
+            Vector3 direccion=(player.position - transform.position).normalized;
+            float distancia = Vo * CurrentTime + (aceleracion * CurrentTime * CurrentTime);
+            Vector3 posicion = transform.position+ direccion*distancia;
+            rb.MovePosition(posicion);
+            CurrentTime +=Time.deltaTime;
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
-    private void FixedUpdate()
+    private float CalculateVelocidadInicial()
     {
-        
+        Vector3 direccion = (player.position - transform.position).normalized;
+        float distancia = Vector3.Distance(transform.position, player.position);
+        return (distancia - (aceleracion * Mathf.Pow(TimeToArrive, 2))) / TimeToArrive;
     }
 }
